41d9831e460e3b47a1f28fd842ad4fc145b2faae
Musaab2003 <musaabyoosuf19@gmail.com>
2026-08-05 13:30:53 +0530

    Expand district filter dropdowns across available items and donor catalogue to all 25 Sri Lankan districts

--- donor.php
+++ donor.php
@@ -188,13 +188,32 @@
                 <div>
                     <label class="form-label" style="font-size: 0.75rem;">District / Location</label>
                     <select id="filterReqDistrict" class="form-control form-select">
-                        <option value="all">All Districts</option>
+                        <option value="all">All Districts (25)</option>
+                        <option value="Ampara">Ampara</option>
+                        <option value="Anuradhapura">Anuradhapura</option>
+                        <option value="Badulla">Badulla</option>
+                        <option value="Batticaloa">Batticaloa</option>
                         <option value="Colombo">Colombo</option>
+                        <option value="Galle">Galle</option>
                         <option value="Gampaha">Gampaha</option>
+                        <option value="Hambantota">Hambantota</option>
+                        <option value="Jaffna">Jaffna</option>
                         <option value="Kalutara">Kalutara</option>
                         <option value="Kandy">Kandy</option>
-                        <option value="Galle">Galle</option>
-                        <option value="Jaffna">Jaffna</option>
+                        <option value="Kegalle">Kegalle</option>
+                        <option value="Kilinochchi">Kilinochchi</option>
+                        <option value="Kurunegala">Kurunegala</option>
+                        <option value="Mannar">Mannar</option>
+                        <option value="Matale">Matale</option>
+                        <option value="Matara">Matara</option>
+                        <option value="Moneragala">Moneragala</option>
+                        <option value="Mullaitivu">Mullaitivu</option>
+                        <option value="Nuwara Eliya">Nuwara Eliya</option>
+                        <option value="Polonnaruwa">Polonnaruwa</option>
+                        <option value="Puttalam">Puttalam</option>
+                        <option value="Ratnapura">Ratnapura</option>
+                        <option value="Trincomalee">Trincomalee</option>
+                        <option value="Vavuniya">Vavuniya</option>
                     </select>
                 </div>
                 <div>
